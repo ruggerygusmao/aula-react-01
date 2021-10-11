376a278161db144cf754c1cf5e0660d54d68c486
ruggerygusmao <ruggery402@gmail.com>
2021-10-11 13:57:19 -0300

    primeira aula de react

---
+++ ola-mundo/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aprendendo React</h1>
+        <img src="https://www.iguiecologia.com/wp-content/uploads/2021/02/Coruja-buraqueira7-2.jpg" alt="coruja" />
+        <pre>
+        Por influência da mitologia grega, tanto que Atena, deusa da 
+        guerra e da sabedoria, tinha uma coruja como mascote. 
+        </pre>
+  
+    </div>
+  );
+}
+
+export default App;
